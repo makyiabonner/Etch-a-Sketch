a363ea1773864419f43f010217b7fbcd8410617c
makyia bonner <makyiabonner@gmail.com>
2022-11-26 18:06:36 -0500

    Increased size of buttons

---
+++ index.js
@@ -0,0 +1,7 @@
+let colorPicker = "colorPicker";
+let colorBtn = document.getElementById("colorBtn");
+
+
+colorBtn.addEventListener(onclick, ){
+    document.getElementById("colorBtn").style.color = "red";
+}
