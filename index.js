db0388a8baac1e4d28e71adb93156cf2b0b8929c
makyia bonner <makyiabonner@gmail.com>
2022-12-10 18:49:14 -0500

    Multi Commits, Preset Grid Visuals, Add Grid

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 let colorPicker = "colorPicker";
 let colorBtn = document.getElementById("colorBtn");
-let sizeValue = document.getElementById("sizeValue")
-let slider = document.getElementById("sizeSlider").oninput = function(){
-    let value = (this.value-this.min)/(this.max-this.min)*64;
-    sizeValue.textContent = `${this.value} x ${this.value}`;
-}
+let rainbowBtn = document.getElementById("rainbowBtn");
+let eraserBtn = document.getElementById("eraserBtn");
+let clearBtn = document.getElementById("clearBtn");
+let slider = document.getElementById("sizeSlider");
+let sizeValue = document.getElementById("sizeValue");
+let sizeSlider = document.getElementById("sizeSlider")
+let grid = document.getElementById("grid");
+const divSpace = document.createElement('div');
+
+
+//making slider functional
+slider.addEventListener("input", function setupGrid(){
+    sizeValue.textContent = `${this.value} x ${this.value}`; 
+    grid.style.gridTemplateColumns = `repeat(${this.value},1fr)`;
+    grid.style.gridTemplateRows = `repeat(${this.value}, 1fr)`;
+    let a = this.value * this.value;
+    
+    for(let i = 0; i < a; i++){   
+        const divSpace = document.createElement('div');
+         divSpace.className = "gridDivs";
+          grid.append(divSpace);
+        };
+        console.log(a);
+    });
+    
+//using slider to add blocks to grid
+
+//making modes clicked recognizeable to host
